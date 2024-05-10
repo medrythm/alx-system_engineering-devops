50028429bff7fc8ac9596c034eaf72a0392a9956
medrythm <developperw@gmail.com>
2024-05-10 13:00:12 +0100

    .

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# A puppet script to fix php settings file by replacing typos
+# Fixing Apache returning a 500 error
 
-exec { 'fix_typo':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/bin','/usr/bin']
+exec { 'fix error':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
